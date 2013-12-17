40cb7952e625dbaad9b8dd4fb0b09255e9f800a1
Trevor Basinger <trevor.basinger@gmail.com>
2013-12-17 10:55:15 -0600

    Added submission class to library

---
+++ lib/shotshare.rb
@@ -0,0 +1,1 @@
+require_relative 'shotshare/submission'
---
+++ lib/shotshare/submission.rb
@@ -0,0 +1,11 @@
+module Shotshare
+
+  # A submission is a container that holds the IDs of screenshots submitted by
+  # a user. There can be multiple screenshots attached to a single submission.
+  # Other records, such as user configs and programs are also associated with
+  # a submission.
+
+  class Submission
+
+  end
+end
